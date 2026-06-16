c85964ab2f7b2c3aa7f75fdd7208df4e36cc4ade
AlexandreKupfermunz <akupfermunz@icloud.com>
2026-06-16 19:56:20 +0200

    Add Individual class with basic parameters

---
+++ individual.py
@@ -0,0 +1,19 @@
+class Individual:
+
+    def __init__(self, impulse_candles, max_duration_ms, take_profit_ticks, 
+                 stop_loss_ticks, min_imbalances):
+        self.impulse_candles = impulse_candles
+        self.max_duration_ms = max_duration_ms
+        self.take_profit_ticks = take_profit_ticks
+        self.stop_loss_ticks = stop_loss_ticks
+        self.min_imbalances = min_imbalances
+        self.fitness = 0
+
+    def print_parameters(self):
+        print(f"Impulse Candles: {self.impulse_candles}")
+        print(f"Max duration: {self.max_duration_ms} ms")
+        print(f" Take profit: {self.take_profit_ticks} ticks")
+        print(f"Stop loss: {self.stop_loss_ticks} ticks")
+        print(f"Min imbalances: {self.min_imbalances}")
+        print(f"Fitness : {self.fitness}")
+
