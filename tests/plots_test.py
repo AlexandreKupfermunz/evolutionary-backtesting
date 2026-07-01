13b21a4e6075fdbe19c23e5b5c4bace70eaade96
AlexandreKupfermunz <akupfermunz@icloud.com>
2026-07-01 16:25:34 +0200

    Add initial plotting test script

---
+++ tests/plots_test.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+from analysis.results_loader import load_all_csvs
+
+from analysis.plot import plot_ga_convergence
+from analysis.plot import plot_train_vs_test
+
+
+results_folder = Path("results_1")
+
+walk_forward_df = load_all_csvs(results_folder, "walk_forward_results.csv")
+generation_train_df = load_all_csvs(results_folder, "generation_train_results.csv")
+generation_test_df = load_all_csvs(results_folder, "generation_test_results.csv")
+
+print(generation_train_df.shape)
+print(generation_train_df.columns)
+
+filtered_train_df = generation_train_df[
+    (generation_train_df["walk_forward_type"] == "expanding") &
+    (generation_train_df["fitness_function"] == "balanced_fitness") &
+    (generation_train_df["repetition"] == "rep_1") 
+]
+
+first_train_start = filtered_train_df["train_start"].min()
+first_train_end = filtered_train_df["train_end"].min()
+
+
+## TODO : change with window_id
+filtered_train_df = filtered_train_df[
+    (filtered_train_df["train_start"] == first_train_start) &
+    (filtered_train_df["train_end"] == first_train_end)
+]
+
+filtered_test_df = generation_test_df[
+    (generation_test_df["walk_forward_type"] == "expanding") &
+    (generation_test_df["fitness_function"] == "balanced_fitness") &
+    (generation_test_df["repetition"] == "rep_1") 
+]
+
+first_test_start = filtered_test_df["train_start"].min()
+first_test_end = filtered_test_df["train_end"].min()
+
+
+## TODO : change with window_id
+filtered_test_df = filtered_test_df[
+    (filtered_test_df["train_start"] == first_test_start) &
+    (filtered_test_df["train_end"] == first_test_end)
+]
+
+
+plot_ga_convergence(filtered_train_df, "net_profit")
+plot_train_vs_test(filtered_train_df, filtered_test_df, "net_profit")
